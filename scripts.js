0b91d940985e5c6811619a58353502844983ebd2
haziqshifa <140826914+haziqshifa@users.noreply.github.com>
2023-12-08 12:46:31 +0800

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,20 @@
+// You can write your JavaScript/jQuery code here
+$(document).ready(function() {
+    // Example: Open a dialog
+    $("#dialog").dialog();
+
+    // Example: Make a call
+    $("#callBtn").click(function() {
+        // Your code to initiate a call
+    });
+
+    // Example: Send email
+    $("#emailBtn").click(function() {
+        // Your code to send an email
+    });
+
+    // Example: Send WhatsApp message
+    $("#whatsappBtn").click(function() {
+        // Your code to send a WhatsApp message
+    });
+});
